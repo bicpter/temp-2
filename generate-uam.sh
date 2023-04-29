0b123eb22cff5011ce8bf0116c4ebdf50f49a33d
jania7fpbb77 <110393806+jania7fpbb77@users.noreply.github.com>
2023-04-29 19:37:54 +0700

    Update generate-uam.sh

--- generate-uam.sh
+++ generate-uam.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo chmod 666 /var/run/docker.sock
 number=$(docker ps | grep debian:bullseye-slim | wc -l)
 docker rm -f $(docker ps -aq --filter ancestor=debian:bullseye-slim) && sudo rm -rf /opt/uam_data
 file_name=$number-docker-compose.yml && rm entrypoint.sh && rm $file_name
