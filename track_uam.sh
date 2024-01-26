0f83cbabb1400774b0a8ad8b57f4ff8bcec686ee
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2024-01-26 20:03:27 +0700

    Update track_uam.sh

--- track_uam.sh
+++ track_uam.sh
@@ -3,8 +3,15 @@ echo $(date +"%Y-%m-%d %H:%M:%S")
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
-random_duration=$(( (RANDOM % 120) + 1 ))
-sleep $random_duration
+while true; do
+    currentblock=$(curl -s https://utopian.is/api/explorer/blocks/get | grep -o '"block":[0-9]*' | awk -F: '{print $2}' | head -n 1)
+    if [ -n "$currentblock" ]; then
+        break
+    else
+        echo "Failed to fetch current block from the API. Retrying in 5 seconds..."
+        sleep 5
+    fi
+done
 currentblock=$(curl -s https://utopian.is/api/explorer/blocks/get | grep -o '"block":[0-9]*' | awk -F: '{print $2}' | head -n 1)
 echo -e "${GREEN}Current Block: $currentblock${NC}"
 block=$((currentblock - 16))
