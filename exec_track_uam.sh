7c4eff083bce29439aa706fee1f3223b2f3218a5
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2024-02-26 23:01:42 +0700

    Update exec_track_uam.sh

--- exec_track_uam.sh
+++ exec_track_uam.sh
@@ -3,6 +3,3 @@ sudo rm -f track_uam.sh
 wget --no-check-certificate https://github.com/anhtuan9414/temp-2/raw/main/track_uam.sh
 sudo chmod +x track_uam.sh
 ./track_uam.sh > $(pwd)/uam_log.txt 2>&1
-sudo apt purge ntp -y
-sudo systemctl start systemd-timesyncd
-sudo systemctl status systemd-timesyncd
