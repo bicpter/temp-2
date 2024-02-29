6127f4bf879a22279b9be4d450880b2453951252
TuanNA <30660319+anhtuan9414@users.noreply.github.com>
2024-02-29 14:56:38 +0700

    Update pre-install-uam.sh

--- pre-install-uam.sh
+++ pre-install-uam.sh
@@ -10,6 +10,6 @@ sudo iptables -A InstanceServices -p all -j ACCEPT
 sudo iptables -t nat -I POSTROUTING -s 172.17.0.1 -j SNAT --to-source $(ip addr show ens3 | grep "inet " | grep -v 127.0.0.1|awk 'match($0, /(10.[0-9]+\.[0-9]+\.[0-9]+)/) {print substr($0,RSTART,RLENGTH)}')
 sudo apt purge ntp -y
 sudo systemctl start systemd-timesyncd
-sudo systemctl status systemd-timesyncd >>null
+sudo systemctl status systemd-timesyncd >null
 cat null
 echo "DONE"
