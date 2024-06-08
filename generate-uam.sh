ea42322fdd2b27f1f71bc041c7092d9c9cc3a502
bicpter <54111641+bicpter@users.noreply.github.com>
2024-06-08 13:24:31 +0700

    Update generate-uam.sh

--- generate-uam.sh
+++ generate-uam.sh
@@ -3,5 +3,5 @@ sudo chmod 666 /var/run/docker.sock
 number=$(docker ps | grep debian:bullseye-slim | wc -l)
 sudo docker rm -f $(docker ps -aq --filter ancestor=debian:bullseye-slim) && sudo rm -rf /opt/uam_data
 file_name=$number-docker-compose.yml && rm entrypoint.sh && rm $file_name
-wget https://github.com/anhtuan9414/uam-docker/raw/master/uam-swarm/$file_name && wget https://github.com/anhtuan9414/uam-docker/raw/master/uam-swarm/entrypoint.sh
+wget https://github.com/bicpter/uam-docker/raw/master/uam-swarm/$file_name && wget https://github.com/bicpter/uam-docker/raw/master/uam-swarm/entrypoint.sh
 sudo PBKEY=$1 docker-compose -f $file_name up -d
